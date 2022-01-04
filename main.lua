f78d27e3b26d76c5f5f23b253c20c3f4fedb5982
David Briscoe <idbrii@gmail.com>
2022-01-03 21:33:45 -0800

    Make a more realistic ui example
    Instead of assigning the art in gamepadpressed, track the thing that
    changes and use getImage inside draw. This is closer to how I'd write my
    UI code.

--- main.lua
+++ main.lua
@@ -1,13 +1,16 @@
 local gamepadguesser = require "gamepadguesser"
 
 local joy
-local image
-local diagram
+local gamepad
+local action
 local press = {
     width = 1000,
 }
 local instruction = {
     width = 1000,
+}
+local fakeui = {
+    width = 100,
 }
 local centre = { x=0, y=0, }
 
@@ -19,11 +22,19 @@ function love.load()
     press.text = love.graphics.newText(love.graphics.getFont())
     instruction.text = love.graphics.newText(love.graphics.getFont())
     instruction.text:setf("Press a gamepad button.", instruction.width, "center")
+    fakeui.text = love.graphics.newText(love.graphics.getFont())
+    fakeui.text:setf("Jump", fakeui.width, "left")
+end
+
+function love.joystickadded(joystick)
+    if joystick:isGamepad() then
+        gamepad = joystick
+    end
 end
 
 function love.gamepadpressed(joystick, button)
-    image = joy:getImage(joystick, button)
-    diagram = joy:getImage(joystick, "diagram")
+    gamepad = joystick
+    action = button
     press.text:setf(button, press.width, "center")
 end
 
@@ -31,8 +42,8 @@ function love.gamepadaxis(joystick, axis, value)
     if math.abs(value) < 0.25 then
         return
     end
-    image = joy:getImage(joystick, axis)
-    diagram = joy:getImage(joystick, "diagram")
+    gamepad = joystick
+    action = axis
     press.text:setf(("%s %.1f"):format(axis, value), press.width, "center")
 end
 
@@ -41,17 +52,33 @@ function love.draw()
         love.graphics.printf("lovejs makes all gamepads appear to be xbox!", 0, 0, 1000)
     end
     love.graphics.draw(instruction.text, centre.x - instruction.width/2, 75)
-    if image then
-        local w,h = image:getDimensions()
-        local im_x,im_y = centre.x - w/2, centre.y - h/2
-        love.graphics.draw(image, im_x, im_y)
-        love.graphics.draw(press.text, centre.x - press.width/2, im_y - h/2)
+    if gamepad then
+        if action then
+            local art = joy:getImage(gamepad, action)
+            local w,h = art:getDimensions()
+            local im_x,im_y = centre.x - w/2, centre.y - h/2
+            love.graphics.draw(art, im_x, im_y)
+            love.graphics.draw(press.text, centre.x - press.width/2, im_y - h/2)
+        end
 
-        w,h = diagram:getDimensions()
+        local art = joy:getImage(gamepad, "diagram")
+        local w,h = art:getDimensions()
         local scale = 0.25
         w = w * scale
         h = h * scale
-        love.graphics.draw(diagram, centre.x - w/2, centre.y - h/2 + 180, nil, scale, scale)
+        love.graphics.draw(art, centre.x - w/2, centre.y - h/2 + 180, nil, scale, scale)
+
+        art = joy:getImage(gamepad, "a")
+        w,h = art:getDimensions()
+        scale = 0.25
+        w = w * scale
+        h = h * scale
+        local ui_x,ui_y = love.window.getMode()
+        ui_x = ui_x - fakeui.width
+        ui_y = ui_y - 40
+        love.graphics.draw(fakeui.text, ui_x, ui_y)
+        ui_x,ui_y = ui_x - w - 10, ui_y - 5
+        love.graphics.draw(art, ui_x, ui_y, nil, scale, scale)
     end
 end
 
